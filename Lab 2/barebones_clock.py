ffda79a6377475502ce74a492c15cf72bcb9fd97
kaejercho <kj@kjcho.io>
2021-03-13 23:42:44 -0500

    update for lab-2

--- Lab 2/barebones_clock.py
+++ Lab 2/barebones_clock.py
@@ -73,39 +73,73 @@ timer_start = 0
 timer_end = 0
 x1 = 50
 x2 = 35
+
+
+
+image_sun = Image.open("sun.jpg")
+image_moon = Image.open("moon.jpeg")
+# Scale the image to the smaller screen dimension
+image_sun_ratio = image_sun.width / image_sun.height
+screen_ratio = width / height
+if screen_ratio < image_sun_ratio:
+    scaled_width = image_sun.width * height // image_sun.height
+    scaled_height = height
+else:
+    scaled_width = width
+    scaled_height = image_sun.height * width // image_sun.width
+new_width = (int(scaled_width*0.2))
+new_height = (int(scaled_height*0.2))
+
+image_sun = image_sun.resize((new_width, new_height), Image.BICUBIC)
+image_moon = image_moon.resize((new_width, new_height), Image.BICUBIC)
+# Crop and center the image
+# x = scaled_width // 2 - width // 2
+# y = scaled_height // 2 - height // 2
+# x,y = 0,0
+# image = image.crop((x, y,x + width, y + height))
+x,y = height - new_height - 1, width - new_width - 1
+offset_x = 0.5
+offset_y = 1
+status = 'day'
+timer_start = time.time()
 while True:
+    # disp.image(image)
     # Draw a black filled box to clear the image.
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    fill = color565(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    timer_end = time.time()
+    timer_str = "{:.4f}".format((timer_end - timer_start))
+    draw.text((0, -2), timer_str, font=font, fill=fill)
 
-    time_str = time.strftime("%m/%d/%Y \n%H:%M:%S")
-    y = top
-    fill = ""
-    if buttonA.value == True:
-        fill = "#FFFFFF"
-    else:
-        fill = color565(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-    draw.text((x1, y), time_str, font=font, fill=fill)
 
-    if buttonB.value == False:
-        if timer == False:
-            timer = True
-            timer_start = time.time()
-            timer_end = timer_start
+    if x > -new_width + 5 :
+        x -= offset_x
+    # else:
+    #     x += 1
+
+    if y > -new_height + 5 :
+        # print(x**2)
+        y -= offset_y
+        # y -= int(x**2 / 1000)
+
+    if x <= -new_width + 5 and  y <= -new_height + 5:
+        if status == "day" :
+            status = "night"
         else:
-            timer_end = time.time()
-        timer_str = "Timer: {:.4f}".format((timer_end - timer_start))
-        y += 3 * font.getsize(time_str)[1]
-        draw.text((x2, y), timer_str, font=font, fill=fill)
+            status = "day"
+
+        x, y = height - new_height - 1, width - new_width - 1
+
+
+    # else:
+    #     y += 2
+    new_x = int(x)
+    new_y = int(y)
+    if status == "day":
+        disp.image(image_sun, rotation,new_x,new_y)
     else:
-        if timer_end - timer_start != 0:
-            timer_str = "Record: {:.4f}".format((timer_end - timer_start))
-            y += 3 * font.getsize(time_str)[1]
-            draw.text((x2, y), timer_str, font=font, fill=fill)
+        disp.image(image_moon, rotation, new_x, new_y)
 
-        timer = False
-
-
-
-    # Display image.
-    disp.image(image, rotation)
-    time.sleep(0.1)
+    if buttonA.value == False:
+        disp.image(image, rotation)
+    time.sleep(0.001)
